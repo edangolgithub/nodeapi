10b8fdf879f0c0509c07ef970a3715cf6f3e9290
edangolgithub <dangolevn@gmail.com>
2021-05-11 10:50:14 +0000

    uytguy

--- nodeapi/modules/ses.js
+++ nodeapi/modules/ses.js
@@ -1,65 +1,95 @@
 const awsses = require("aws-sdk/clients/ses");
 const nodemailer = require("nodemailer");
 
-fs = require('fs')
-const ses= new awsses({ region: "us-east-1" });
+fs = require("fs");
+const ses = new awsses({ region: "us-east-1" });
 
-module.exports.sendemailwithattachment =async  (files,email,message) => {
-    const fileContent = Buffer.from(files.file.data, 'binary');
-//console.log(fileContent)
-//console.log(files)
-console.log(ses)
+module.exports.sendemailwithattachment = async (files, email, message) => {
+  if (files) {
+    const fileContent = Buffer.from(files.File.data, "binary");
+    //console.log(fileContent)
+    //console.log(files)
+
+    console.log(ses);
     var mailOptions = {
-        from: 'dangolevan@gmail.com',
-        subject: 'this is contact us test',
-        html: `<p>You got a contact message from: <b>${email}</b>
+      from: "dangolevan@gmail.com",
+      subject: "this is contact us test",
+      html: `<p>You got a contact message from: <b>${email}</b>
         message: <p>${message}</p>
         </p>`,
-        to: 'dangolevan@gmail.com',
-        // bcc: Any BCC address you want here in an array,
-        attachments: [
-            {
-                filename: files.file.name,
-                content: fileContent,
-                ContentType: files.file.mimetype               
-            }
-        ]
+      to: "dangolevan@gmail.com",
+      // bcc: Any BCC address you want here in an array,
+      attachments: [
+        {
+          filename: files.File.name,
+          content: fileContent,
+          ContentType: files.File.mimetype,
+        },
+      ],
     };
 
-    console.log('Creating SES transporter');
+    console.log("Creating SES transporter");
     // create Nodemailer SES transporter
     var transporter = nodemailer.createTransport({
-        SES: ses
+      SES: ses,
     });
 
     // send email
- const resp = await new Promise((rsv, rjt) => {
-transporter.sendMail(mailOptions, function (error, info) {
+    const resp = await new Promise((rsv, rjt) => {
+      transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            return rjt(error)
-        } 
-        console.log(info)
-        rsv('Email sent'); 
+          return rjt(error);
+        }
+        console.log(info);
+        rsv("Email sent");
+      });
     });
-});
 
-return await response(resp);
+    return await response(resp);
+  } else {
+    console.log(ses);
+    var mailOptions = {
+      from: "noreply@ecolawnlandscaping.tk",
+      subject: "Some one Contacted Eco-Friendly Lawn And landscaping",
+      html: `<p>You got a contact message from: <b>${email}</b>
+        message: 
+        
+        <p>${message}</p>
+        </p>`,
+      to: "dangolevan@gmail.com,mahesh@ecolawnlandscaping.com",
+    };
 
+    console.log("Creating SES transporter");
+    // create Nodemailer SES transporter
+    var transporter = nodemailer.createTransport({
+      SES: ses,
+    });
 
+    // send email
+    const resp = await new Promise((rsv, rjt) => {
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return rjt(error);
+        }
+        console.log(info);
+        rsv("Email sent");
+      });
+    });
 
+    return await response(resp);
+  }
 };
-
 
 async function response(data) {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      'Access-Control-Allow-Credentials': true // Required for CORS support to work
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for CORS support to work
     },
     body: JSON.stringify(
       {
-       data: data,
+        data: data,
       },
       null,
       2
--- nodeapi/server.js
+++ nodeapi/server.js
@@ -1,117 +1,176 @@
-'use strict';
+"use strict";
 
-const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
-var cors = require('cors')
-app.use(cors())
-const fileUpload = require('express-fileupload');
-const evans3 = require('./modules/evans3.js')
-const ses = require('./modules/ses')
+const awsses = require("aws-sdk/clients/ses");
 
-app.use(fileUpload({
-  createParentPath: true
-}));
+const ses1 = new awsses({ region: "us-east-1" });
+
+var cors = require("cors");
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+const fileUpload = require("express-fileupload");
+const evans3 = require("./modules/evans3.js");
+const ses = require("./modules/ses");
+
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-app.get('/createbucket/:bucketname', (req, res) => {
-  console.log(req)
-  var data = es33.createBucket(req.params.bucketname)
-  console.log(data)
-  res.send("ok")
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-})
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-app.get('/listbuckets', async function (req, res) {
-  var data = await evans3.listbuckets()
-  console.log(data)
-  res.json({ "data": data });
+  // Pass to next layer of middleware
+  next();
+});
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/createbucket/:bucketname", (req, res) => {
+  console.log(req);
+  var data = es33.createBucket(req.params.bucketname);
+  console.log(data);
+  res.send("ok");
 });
 
-app.get('/listobjects/:bucketname', async function (req, res) {
-  var data = await evans3.listobjects(req.params.bucketname)
-  console.log(data)
-  res.json({ "data": data });
-});
-app.get('/listobjectsv2/:bucketname', async function (req, res) {
-  var data = await evans3.listobjectsv2(req.params.bucketname)
-  console.log(data)
-  res.json({ "data": data });
+app.get("/listbuckets", async function (req, res) {
+  var data = await evans3.listbuckets();
+  console.log(data);
+  res.json({ data: data });
 });
 
+app.get("/listobjects/:bucketname", async function (req, res) {
+  var data = await evans3.listobjects(req.params.bucketname);
+  console.log(data);
+  res.json({ data: data });
+});
+app.get("/listobjectsv2/:bucketname", async function (req, res) {
+  var data = await evans3.listobjectsv2(req.params.bucketname);
+  console.log(data);
+  res.json({ data: data });
+});
 
-
-
-
-
-
-app.post('/imageupload', async (req, res) => {
-  const { files } = req
+app.post("/imageupload", async (req, res) => {
+  const { files } = req;
   //console.log(req)
   if (!req.files) {
     res.send({
       status: false,
-      message: 'No file uploaded'
+      message: "No file uploaded",
     });
     return;
   }
   const data = await evans3.uploadimage(files, req.body.folder);
   res.send({
-    "response_code": 200,
-    "response_message": "Success",
-    "response_data": data
+    response_code: 200,
+    response_message: "Success",
+    response_data: data,
   });
-})
-app.post('/fileupload', async (req, res) => {
-  const { files } = req
+});
+app.post("/fileupload", async (req, res) => {
+  const { files } = req;
   //console.log(req)
   if (!req.files) {
     res.send({
       status: false,
-      message: 'No file uploaded'
+      message: "No file uploaded",
     });
     return;
   }
   var bucketname = "";
   if (!req.body.bucket) {
-    bucketname = "ed2021"
-  }
-  else {
-    bucketname = req.body.bucket
+    bucketname = "ed2021";
+  } else {
+    bucketname = req.body.bucket;
   }
   const data = await evans3.fileupload(files, bucketname);
   res.send({
-    "response_code": 200,
-    "response_message": "Success",
-    "response_data": data
+    response_code: 200,
+    response_message: "Success",
+    response_data: data,
   });
-})
+});
 
-app.post('/sendemailwithattachment', async (req, res) => {
+app.post("/sendemailwithattachment", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
 
-  const { files } = req
-  
-  var emailAddress = req.email
-  var message = req.message
-  console.log(req);
-  console.log(req);
-  const data = await  ses.sendemailwithattachment(files, emailAddress, message);
+  const { files,email,message } = req;
+
+  console.log(req.body.email);
+  const data = await ses.sendemailwithattachment(files, req.body.email, req.body.message);
+
   res.send({
-     "headers": {
-           
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
-        },
-    "response_code": 200,
-    "response_message": "Success",
-    "response_data": data
+    data,
   });
-})
+});
+app.post("/sendemail", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
+
+  var img = req.body;
+  var email = req.email;
+  var message = req.message;
+  const params = {
+    Destination: {
+      ToAddresses: ["dangolevan@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: `You just got a message from "customer" - ${req.email}:
+          ${req.message}`,
+        },
+      },
+      Subject: { Data: "data" },
+    },
+    Source: "admin@ecolawnlandscaping.tk",
+    ReplyToAddresses: req.replyaddresses,
+  };
+  var data = await ses1.sendEmail(params).promise();
+  res.send({ d: data });
+});
 
 module.exports.start = serverless(app);
-
